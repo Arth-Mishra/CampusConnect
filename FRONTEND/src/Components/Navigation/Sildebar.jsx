eef97623dc95a8cb84327b654d9d352f0c41d569
Arth Mishra <arthmishra1504@gmail.com>
2024-01-20 18:48:33 +0530

    Backend Folder added

--- FRONTEND/src/Components/Navigation/Sildebar.jsx
+++ FRONTEND/src/Components/Navigation/Sildebar.jsx
@@ -61,24 +61,50 @@ const Sidebar = () => {
                 <span>About</span>
               </div>
               <div>
-                <button
-                  type="button"
-                  className=" hover:scale-105 flex text-sm w-6 h-6 bg-white rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                  aria-expanded="false"
-                  data-dropdown-toggle="dropdown-user"
-                >
-                  {/* <img
-                                        className="w-8 h-8 rounded-full"
-                                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwnYnwftDUSjsQmLQvMBZ2pwDXhAJiIdfKvg&usqp=CAU"
-                                        alt="user photo"
-                                    /> */}
-                  <i className="fa-solid fa-user pt-1 w-12"></i>
-                </button>
+                <div>
+                  <Link to="/profile">
+                    <img
+                      id="dropdownHoverButton"
+                      type="button"
+                      data-dropdown-toggle="dropdownHover"
+                      data-dropdown-trigger="hover"
+                      data-dropdown-placement="bottom-start"
+                      className="w-10 h-10 rounded-full cursor-pointer"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa4AhCCgcbwRr3UyrRRZ8BI6CSwNKCsIFa8TCaHXCYAA&s"
+                      alt="User dropdown"
+                    />
+                  </Link>
+                  {/* <i className="fa-solid fa-user pt-1 w-12"></i> */}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </nav>
+      <div
+                id="dropdownHover"
+                className="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44  dark:bg-gray-700 dark:divide-gray-600"
+            >
+                <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                    <div>Nikhil Sharma</div>
+                    <div className="text-orange-500 font-medium truncate">nksharma@gmail.com</div>
+                </div>
+                <ul className=" text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                            Account
+                        </a>
+                    </li>
+                </ul>
+                <div className="py-1">
+                    <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                        Sign out
+                    </a>
+                </div>
+            </div>
 
       {/* Side Bar  */}
 
